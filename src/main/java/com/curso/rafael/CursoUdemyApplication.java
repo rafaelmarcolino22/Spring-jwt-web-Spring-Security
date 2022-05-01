0b4d9146767443d3e2a9f105bfeb1c8e5bdd1e39
rafa <ralfrr55@gmail.com>
2022-05-01 16:05:23 -0300

    criando operacao de intanciacao

--- src/main/java/com/curso/rafael/CursoUdemyApplication.java
+++ src/main/java/com/curso/rafael/CursoUdemyApplication.java
@@ -1,13 +1,35 @@
 package com.curso.rafael;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoUdemyApplication {
+import com.curso.rafael.domain.Categoria;
+import com.curso.rafael.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoUdemyApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		
+		
 	}
 
 }
