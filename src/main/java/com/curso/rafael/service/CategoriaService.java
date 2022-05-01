e8c209629eb92f4d69380adbe2419de502472a72
rafa <ralfrr55@gmail.com>
2022-05-01 20:21:10 -0300

    endpoint/categorias

--- src/main/java/com/curso/rafael/service/CategoriaService.java
+++ src/main/java/com/curso/rafael/service/CategoriaService.java
@@ -7,16 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.curso.rafael.domain.Categoria;
 import com.curso.rafael.repositories.CategoriaRepository;
+import com.curso.rafael.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	public CategoriaRepository respo;
-	
+
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = respo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 }
