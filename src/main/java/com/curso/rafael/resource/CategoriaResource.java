86b18c42b683ef76a0024b429ab8320efe51a559
rafa <ralfrr55@gmail.com>
2022-05-01 15:29:57 -0300

    categoria dominio

--- src/main/java/com/curso/rafael/resource/CategoriaResource.java
+++ src/main/java/com/curso/rafael/resource/CategoriaResource.java
@@ -1,17 +1,32 @@
 package com.curso.rafael.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.rafael.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "rest OK";
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "mouse");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		
 		
 	}
 
