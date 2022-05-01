3d291b5e5e09ad9bf35d2e5980b232c4c1c34d60
rafa <ralfrr55@gmail.com>
2022-05-01 17:20:17 -0300

    many to many Certo

--- src/main/java/com/curso/rafael/domain/Produto.java
+++ src/main/java/com/curso/rafael/domain/Produto.java
@@ -1,9 +1,7 @@
 package com.curso.rafael.domain;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,21 +12,24 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Produto implements Serializable {
+public class Produto  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
 	
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
-		joinColumns = @JoinColumn(name= "produto_id"),
+		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
-			)
-	private List<Categoria> categorias =  new ArrayList<>();
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
+	}
 
 	public Produto(Integer id, String nome, Double preco) {
 		super();
@@ -71,7 +72,10 @@ public class Produto implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -83,9 +87,11 @@ public class Produto implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Produto other = (Produto) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-
 }
