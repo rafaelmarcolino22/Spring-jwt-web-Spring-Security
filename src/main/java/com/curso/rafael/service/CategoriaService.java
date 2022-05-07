e774f38eafb21e772bfdd0f4c952569fae20496a
rafa <ralfrr55@gmail.com>
2022-05-07 14:11:19 -0300

    Categoria Delete

--- src/main/java/com/curso/rafael/service/CategoriaService.java
+++ src/main/java/com/curso/rafael/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.curso.rafael.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,11 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		
+		return respo.findAll();
 	}
 	
 }
