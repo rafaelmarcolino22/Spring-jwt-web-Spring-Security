f09d3be31b50b9995ccaddc4977eac371fe2e007
rafa <ralfrr55@gmail.com>
2022-05-08 17:23:46 -0300

    cliente get post put delete

--- src/main/java/com/curso/rafael/dto/CategoriaDTO.java
+++ src/main/java/com/curso/rafael/dto/CategoriaDTO.java
@@ -15,9 +15,11 @@ public class CategoriaDTO implements Serializable {
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
+	
+	
 	public CategoriaDTO() {
 	}
 	
--- src/main/java/com/curso/rafael/service/ClienteService.java
+++ src/main/java/com/curso/rafael/service/ClienteService.java
@@ -1,12 +1,19 @@
 package com.curso.rafael.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.curso.rafael.domain.Cliente;
+import com.curso.rafael.dto.ClienteDTO;
 import com.curso.rafael.repositories.ClienteRepository;
+import com.curso.rafael.service.exception.DataIntegrityException;
 import com.curso.rafael.service.exception.ObjectNotFoundException;
 
 @Service
@@ -21,6 +28,40 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return respo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			respo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos");
+		}
+	}
+
+	public List<Cliente> findAll() {
+
+		return respo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return respo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
