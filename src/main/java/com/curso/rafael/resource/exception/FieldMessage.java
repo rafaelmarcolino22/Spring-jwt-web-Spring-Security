b139a56df5cc0057b740f6eba4cf67228517f3a1
rafa <ralfrr55@gmail.com>
2022-05-08 13:13:29 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/curso/rafael/resource/exception/FieldMessage.java
@@ -0,0 +1,36 @@
+package com.curso.rafael.resource.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String messege;
+	
+	public FieldMessage() {
+		
+	}
+
+	public FieldMessage(String fieldName, String messege) {
+		super();
+		this.fieldName = fieldName;
+		this.messege = messege;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessege() {
+		return messege;
+	}
+
+	public void setMessege(String messege) {
+		this.messege = messege;
+	}
+}
